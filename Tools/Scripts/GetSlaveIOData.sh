141072d5b39e522e7bc20a28bd382b559ea1b00d
Marc-Andre Guimond <marcandre.guimond@systemsgrr.com>
2016-06-05 20:38:54 -0400

    Update script SlaveIOData.

--- Tools/Scripts/GetSlaveIOData.sh
+++ Tools/Scripts/GetSlaveIOData.sh
@@ -1,24 +1,15 @@
-EA:6A:97:5D:C3:F0#!/bin/bash
-
-# Initialise le device bluetooth
-#echo "Remise à zéro du device"
-#if [ $(hciconfig hci0 reset) ]; then
-#	exit 1;
-#fi
+#!/bin/bash
 
 BLE_ADDR=$1
 
-# GET_SENSOR_DATA test:
-# <SENSOR_DATA><GET><CHECKSUM>
-# Checksum = ~(DataBytesSum & 0x00FF) = 0x22
-# Packet result: 0xA13C22
-
-if [ $BLE_ADDR ]; then
+if [ "$BLE_ADDR" ]
+    then
         echo "BLE address found: "$BLE_ADDR
         # Enable notification and indication flags
         gatttool -b $BLE_ADDR --char-write-req -a 0x0011 -n 0300
         # Send data packet through the BlueMon Request characteristic
-        gatttool -b $BLE_ADDR --char-write-req -a 0x000E -n A13C22 --listen
+        gatttool -b $BLE_ADDR --char-write-req -a 0x000E -n A23C0023 --listen
 else
     echo "Cannot get BLE address."
 fi
+
